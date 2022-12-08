9010771234697f0d509d30af6517706f21b00065
Obinnat <markobinna65@gmail.com>
2022-12-08 15:37:49 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+
+/**
+ * main- a main file
+ *
+ * Return: success (0)
+ *
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
