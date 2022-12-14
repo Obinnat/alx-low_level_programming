a7d193417644ceee238e902a9ce67925bd73a7c6
Obinnat <markobinna65@gmail.com>
2022-12-13 21:41:46 -0800

    I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,13 +6,16 @@
  * Return: Always 0
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
-	for (a = 0; a <= z; a++)
+	char ob;
 
+	ob = 'a';
+
+	while (ob <= 'z')
 	{
-		_putchar('a');
+		_putchar(ob);
+		ob++;
 	}
 	_putchar('\n');
-	return (0);
 }
